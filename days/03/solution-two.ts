444fa5327166ee87241028029268f7b93e9f929f
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-12-03 19:57:51 +0800

    day 3

--- days/03/solution-two.ts
+++ days/03/solution-two.ts
@@ -1,3 +1,36 @@
+function combinations(arr: number[]) {
+  const result: number[][] = [[]];
+
+  for (const item of arr) {
+    const currentLength = result.length;
+    for (let i = 0; i < currentLength; i++) {
+        const combination = [...result[i]!, item];
+        result.push(combination);
+    }
+  }
+
+  return Math.max(...result
+    .filter(x => x.length === 12)
+    .map(x => Number(x.toString().split(',').join(''))))
+}
+
 export default async function solution(puzzle: string[]): Promise<number> {
-    return 0
+    let answer = 0;
+
+    for (const line of puzzle) {
+        const bank = line.split("").map(Number);
+        let maxJoltage = 0;
+
+        let joltage = combinations(bank);
+        console.log(joltage)
+        // console.log(joltage)
+        // joltage.forEach(x => {
+        //     const n = Number(x.toString().split(',').join(''));
+        //     maxJoltage = Math.max(maxJoltage, n);
+        // });
+
+        answer += maxJoltage;
+    }
+
+    return answer;
 }
