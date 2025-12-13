b83f15fb44fd337d71b4bfbac9e0a56f012624bc
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-12-13 14:18:16 +0800

    day 7

--- days/07/solution-one.ts
+++ days/07/solution-one.ts
@@ -1,62 +1,46 @@
-import { Beam, tui } from "./util";
+import { Beam, BeamManager, tui } from "./util";
 
 export default async function solution(puzzle: string[]) {
   let answer = 0;
   let width = puzzle[0].length;
   let height = puzzle.length;
-  let beams: Beam[] = [new Beam(puzzle[0].indexOf("S"), 1)];
-  let grid = puzzle.map((x) => x.split(""));
-  let visual = new Array(...grid);
+  // let beams: Beam[] = [new Beam(puzzle[0].indexOf("S"), 1)];
+  let beams = new BeamManager();
+  beams.add(puzzle[0].indexOf("S"), 1);
+  // let grid = puzzle.map((x) => x.split(""));
+  let visual = new Array(...puzzle.map((x) => x.split("")));
+  visual[1][puzzle[0].indexOf("S")] = "|";
 
   for (let y = 1; y < height; y++) {
-    beams.forEach((beam) => {
-      visual[beam.y][beam.x] = "|";
-
+    for (const beam of beams.toArray()) {
       const down = beam.y + 1;
       const left = beam.x - 1;
       const right = beam.x + 1;
-      if (grid[down] && grid[down][beam.x] === "^") {
-        answer++;
-        if (left >= 0 && grid[down][left] === ".") {
+
+      if (visual[down] && visual[down][beam.x] === "^") {
+        if (left >= 0 && visual[down][left] === ".") {
+          answer++;
           visual[down][left] = "|";
-          beam.x = left;
-          beam.y = down;
+          beam.update(left, down);
         }
 
-        if (right < width && grid[down][right] === ".") {
+        if (right < width && visual[down][right] === ".") {
           visual[down][right] = "|";
-          beams.push(new Beam(down, right));
+          if(beams.add(right, down)){
+            answer++;
+          }
         }
-
-        // if (right < width && grid[down][right] === '.') {
-        //   beams.push(new Beam(down, right))
-        // }
-        // visual[beam.y + 1][beam.x - 1] = "|";
-        // visual[beam.y + 1][beam.x + 1] = "|";
-
-        // if (left > 0 && gird[beam.y+1][left] !== "^") {
-        //   beams.push(new Beam(down, left))
-        // }
-
-        // if (right < width - 1 && gird[beam.y+1][right] !== "^") {
-        //   beams.push(new Beam(down, right))
-        // }
       }
 
-      if (grid[down] && grid[down][beam.x] === ".") {
+      if (visual[down] && visual[down][beam.x] === ".") {
         visual[down][beam.x] = "|";
-        beam.y = down;
+        beam.update(beam.x, down);
       }
-    });
+    }
 
     tui(visual);
+    // console.log(beams.toArray())
   }
-
-  // for (let y = start.y; y < height; y++) {
-  //   for (let x = start.x; x < width; x++) {
-  //     // console.log(puzzle[y][x])
-  //   }
-  // }
 
   return answer;
 }
--- days/07/util.ts
+++ days/07/util.ts
@@ -11,6 +11,43 @@ export class Beam {
     this.x = x;
     this.y = y;
   }
+
+  update(x:number, y:number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class BeamManager {
+  beams: Map<string, Beam>
+  constructor() {
+    this.beams = new Map<string, Beam>();
+  }
+
+  add(x: number, y: number) {
+    const key = `${x},${y}`;
+    if (!this.beams.has(key)) {
+      this.beams.set(key, new Beam(x, y));
+      return true;
+    }
+    return false;
+  }
+
+  update(x:number, y:number) {
+    this.beams[`${x},${y}`] = this.beams[`${x},${y}`].update(x, y);
+  }
+
+  has(x: number, y: number) {
+    return this.beams.has(`${x},${y}`);
+  }
+
+  remove(x: number, y: number) {
+    return this.beams.delete(`${x},${y}`);
+  }
+
+  toArray() {
+    return Array.from(this.beams.values());
+  }
 }
 
 export function tui(grid: string[][]) {
